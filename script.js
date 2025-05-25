b790107ae6c1815b216f2c23874b18546d8867f6
dudulimavasc84 <lima65913@gmail.com>
2025-05-25 12:59:22 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-<!-- Conteúdo de exemplo para script.js -->
+
+function login(event) {
+  event.preventDefault();
+  const user = document.getElementById("username").value;
+  const pass = document.getElementById("password").value;
+  if (user === "dudu" && pass === "fer111984") {
+    window.location.href = "painel.html";
+  } else {
+    alert("Usuário ou senha incorretos.");
+  }
+}
