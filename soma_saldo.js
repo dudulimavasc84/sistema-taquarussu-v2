ff2d6cd50e49139e577ea8b0f7cb7d417e4517ed
dudulimavasc84 <lima65913@gmail.com>
2025-05-27 14:46:55 -0300

    Add files via upload

---
+++ soma_saldo.js
@@ -0,0 +1,18 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+  const campos = ["inicial", "entrada", "transferencia", "saida", "abate", "mortes"];
+  campos.forEach(id => {
+    document.getElementById(id).addEventListener("input", calcularSaldo);
+  });
+
+  function calcularSaldo() {
+    const inicial = parseInt(document.getElementById("inicial").value) || 0;
+    const entrada = parseInt(document.getElementById("entrada").value) || 0;
+    const transferencia = parseInt(document.getElementById("transferencia").value) || 0;
+    const saida = parseInt(document.getElementById("saida").value) || 0;
+    const abate = parseInt(document.getElementById("abate").value) || 0;
+    const mortes = parseInt(document.getElementById("mortes").value) || 0;
+    const saldo = inicial + entrada - transferencia - saida - abate - mortes;
+    document.getElementById("saldo").value = saldo;
+  }
+});
